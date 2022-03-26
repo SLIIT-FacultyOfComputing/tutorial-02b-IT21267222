7fb44145f6dd1102b60d553fabbbe83c24b5cfda
IT21267222 EDIRISINGHE E.A.K.G <100871086+IT21267222@users.noreply.github.com>
2022-03-26 07:57:29 +0000

    Initial commit

--- tute04.cpp
+++ tute04.cpp
@@ -12,6 +12,7 @@ long nCr(int n, int r);
 Do not modify the main function.*/
 
 #include <iostream>
+using namespace std;
 
 long Factorial(int no);
 long nCr(int n, int r);
@@ -27,3 +28,35 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+
+
+long Factorial(int no){
+  int fac = 1;
+  for(int i=no; i>=1; i--){
+    fac = fac * i;
+  }
+  return fac;
+}
+
+
+long nCr(int num, int x){
+  long factN,factD,factR;
+  factN = Factorial(int no);
+
+  factR = 1;
+  for(int i=x; i>=1; i--){
+    factR = factR * i;
+  }
+  
+  int sub;
+  sub = num - x;
+  factD = 1;
+  for(int i=sub; i>=1; i--){
+    factD = factD * i;
+  }
+
+  return factN / (factD * factR);
+  
+}
+
+
